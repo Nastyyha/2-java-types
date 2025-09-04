2e18799ca9020f392c54653f2cb10ea39fbe8a26
Nastyyha <farlanskihanastasia@gmail.com>
2025-09-04 20:05:24 +0500

    Done

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -6,16 +6,30 @@ public class Task02 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        long solve = Long.parseLong(input);
+        if (Byte.MIN_VALUE <= solve && solve <= Byte.MAX_VALUE) {
+            return "byte";
+        }
+
+
+        else if (Short.MIN_VALUE <= solve && solve <= Short.MAX_VALUE) {
+            return "short";
+        }
+
+        else if (Integer.MIN_VALUE <= solve && solve <= Integer.MAX_VALUE) {
+            return "int";
+        }
+
+        else {
+            return "long";
+        }
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
+
     }
 
 }
